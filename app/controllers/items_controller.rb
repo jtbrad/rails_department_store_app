f1122ac5b9ae207876a2d04f970d69227b65990c
Jacob Bradford <jcbbrdfrd@yahoo.com>
2019-11-13 21:15:54 -0700

    add item CRUD methods to controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,22 +1,62 @@
 class ItemsController < ApplicationController
+  before_action set_department
+  before_action set_item, only: [:show, :edit, :update]
+
   def index
-    @department = Department.find(params[:department_id])
     @items = @department.items
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def new
+    @item = @department.topics.new
+    render partial: "form"
+  end
+
+  def create
+    @item = @department.items.new(item_params)
+
+    if @item.save
+      redirect_to department_items_path(@department)
+    else
+      render :new
+    end
+
   end
 
   def edit
+    render partial: "form"
+  end
+
+  def update
+    
+    if @item.update(item_params)
+      redirect_to department_items_path(@department)
+    else
+      render :edit
+    end
+
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to department_items_path(@department)
   end
 
   private
 
-    def get_department
-    
+    def set_department
+      @department = Department.find(params[:department_id])
+    end
+
+    def set_item
+      @item = Item.find(params[:id])
+    end
+
+    def item_params
+      params.require(:item).permit(:name)
     end
 
 end
